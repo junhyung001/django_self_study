cfc609a6fe3bd55ad1da77bb367d9a4e33e20b4c
junhyung001 <junhyung@gmaail.com>
2023-11-14 13:40:49 +0900

    ADD : crud add

---
+++ article/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import redirect, render
+from article.models import Article
+
+def index(request):
+    articles = Article.objects.all()  # Corrected variable name
+    context = {'articles': articles}  # Corrected variable name
+    print(context)
+    return render(request, 'article/index.html', context)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {'article': article}  # Corrected variable name
+    return render(request, 'article/detail.html', context)
+
+def new(request):
+    return render(request, 'article/new.html')
+
+def create(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    article = Article(title=title, content=content)
+    article.save()
+    
+    return redirect('article:index')
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('article:index')
